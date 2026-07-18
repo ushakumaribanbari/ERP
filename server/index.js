7440af8fa083159b851119e51cbd23e5bfaf9502
Jignesh083 <jigneshprajapati2003@outlook.com>
2026-07-18 15:46:55 +0530

    Fix trust proxy

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 // ── Connect to MongoDB ────────────────────────────────────────
